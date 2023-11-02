eab3cca64fedec72952431490a7be3d01918cb8d
s.feli <s.feli@DESKTOP-8HQAAOS>
2023-11-02 07:33:41 +0330

    save

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ import headerImage from "../Assets/Frame 39.png";
 
 import classes from './Home.module.css';
 
-const Home = props => {
+const Home = () => {
 
     const videoRefOne = useRef(null);
     const innerVideoRef = useRef(null);
@@ -23,13 +23,10 @@ const Home = props => {
     const frameRef = useRef(null);
     const frameHeaderRef = useRef(null);
     const frameFooterRef = useRef(null);
+    const startRef = useRef(null);
+    const mobileSectionRef = useRef(null);
 
-    const [scroll, setScroll] = useState(0);
-    const [screenHeight, setScreenHeight] = useState(0);
-    const [frameHeaderHeight, setFrameHeaderHeight] = useState(0);
-    const [frameFooterHeight, setFrameFooterHeight] = useState(0);
-    const [framePostsHeight, setFramePostsHeight] = useState(0);
-    const [frameHeight, setFrameHeight] = useState(0);
+    const [hideFrameVideo,setHideFrameVideo] = useState(false)
 
     useEffect(() => {
         videoRefOne?.current?.play();
@@ -39,65 +36,86 @@ const Home = props => {
         innerVideoRef?.current?.play();
     }, [innerVideoRef]);
 
-
     const watchScroll = () => {
-        setScroll(window.scrollY);
+        
+        const frameHeaderHeight = frameHeaderRef.current.offsetHeight;
+        const frameFooterHeight = frameFooterRef.current.offsetHeight;
+        const framePostsHeight = postsRef.current.offsetHeight;
+        const frameHeight = frameRef.current.offsetHeight;
+        const screenHeight = window.innerHeight;
+
+        scroll = window.scrollY;
+        let startVideoOpacity = 1;
+        if (scroll > screenHeight / 2) {
+            startVideoOpacity = 0;
+        } else {
+            startVideoOpacity = (screenHeight - 2 * scroll) / screenHeight
+        }
+
+        startRef.current.style.opacity = startVideoOpacity;
+        startRef.current.style.visibility =  startVideoOpacity ? 'visible' : 'hidden';
+
+        let mobileSectionStyle = {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            marginTop: 0
+        };
+
+        let frameScale = 5;
+        if (2 * scroll > screenHeight) {
+            frameScale = 1;
+        } else {
+            frameScale = 4 * (screenHeight - 2 * scroll) / screenHeight + 1
+        }
+        let postsTop = frameHeaderHeight;
+        postsTop = frameHeaderHeight - (scroll - screenHeight / 2);
+    
+        if (framePostsHeight && scroll > framePostsHeight - frameHeight + frameFooterHeight + frameHeaderHeight + screenHeight / 2) {
+            postsTop = frameHeight - frameFooterHeight - framePostsHeight;
+            
+            mobileSectionStyle = {
+                position: 'relative',
+                top: 0,
+                left: 0,
+                right: 0,
+                marginTop: framePostsHeight - frameHeaderHeight + frameFooterHeight + 'px'
+            };
+        }
+        
+        mobileSectionRef.current.style.position = mobileSectionStyle.position;
+        mobileSectionRef.current.style.top = mobileSectionStyle.top;
+        mobileSectionRef.current.style.left = mobileSectionStyle.left;
+        mobileSectionRef.current.style.right = mobileSectionStyle.right;
+        mobileSectionRef.current.style.marginTop = mobileSectionStyle.marginTop;
+
+        frameRef.current.style.transform = "scale("+frameScale+")";
+        frameRef.current.style.opacity =  1 - startVideoOpacity;
+
+        postsRef.current.style.top = postsTop + 'px';
+
+        if (scroll > (screenHeight / 2)){
+            setHideFrameVideo(true)
+        }else{
+            setHideFrameVideo(false)
+        }
     }
 
     useEffect(() => {
-        setTimeout(() => {
-            setFrameHeaderHeight(frameHeaderRef.current?.offsetHeight);
-            setFrameFooterHeight(frameFooterRef.current?.offsetHeight);
-            setFramePostsHeight(postsRef.current?.offsetHeight);
-            setFrameHeight(frameRef.current?.offsetHeight);
-        }, [100]);
 
         watchScroll();
+
         window.addEventListener('scroll', watchScroll);
-        setScreenHeight(window.innerHeight);
         return (() => {
             window.removeEventListener('scroll', watchScroll);
         })
     }, []);
 
-    let startVideoOpacity = 1;
-    if (scroll > screenHeight / 2) {
-        startVideoOpacity = 0;
-    } else {
-        startVideoOpacity = (screenHeight - 2 * scroll) / screenHeight
-    }
-
-    let mobileSectionStyle = {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0
-    };
-
-    let frameScale = 5;
-    if (2 * scroll > screenHeight) {
-        frameScale = 1;
-    } else {
-        frameScale = 4 * (screenHeight - 2 * scroll) / screenHeight + 1
-    }
-    let postsTop = frameHeaderHeight;
-    postsTop = frameHeaderHeight - (scroll - screenHeight / 2);
-
-    if (scroll > framePostsHeight - frameHeight + frameFooterHeight + frameHeaderHeight + screenHeight / 2) {
-        postsTop = frameHeight - frameFooterHeight - framePostsHeight;
-        mobileSectionStyle = {
-            position: 'relative',
-            top: 0,
-            left: 0,
-            right: 0,
-            marginTop: framePostsHeight - frameHeaderHeight + frameFooterHeight + 'px'
-        };
-
-    }
 
     return (
         <div>
-            <div className={classes.videoWrapper} style={{ opacity: startVideoOpacity, visibility: startVideoOpacity ? 'visible' : 'hidden' }}>
+            <div className={classes.videoWrapper} ref={startRef} >
                 <video
                     className={classes.startVideo}
                     playsInline
@@ -114,13 +132,13 @@ const Home = props => {
             </div>
 
             <div className={classes.main} >
-                <div className={classes.mobileSection} style={mobileSectionStyle}>
-                    <div className={classes.mobileWRapper} ref={frameRef} style={{ transform: `scale(${frameScale})`, opacity: 1 - startVideoOpacity }}>
-                        <img src={posts} alt="" className={classes.posts} ref={postsRef} style={{ top: postsTop }} />
+                <div className={classes.mobileSection} ref={mobileSectionRef} >
+                    <div className={classes.mobileWRapper} ref={frameRef} >
+                        <img src={posts} alt="" className={classes.posts} ref={postsRef} />
                         <img src={headerImage} alt="" className={classes.frameHeader} ref={frameHeaderRef} />
                         <img src={footerImage} alt="" className={classes.frameFooter} ref={frameFooterRef} />
                         <video
-                            className={`${classes.innerVideo} ${scroll > (screenHeight / 2) ? classes.hide : ''}`}
+                            className={`${classes.innerVideo} ${hideFrameVideo ? classes.hide : ''}`}
                             playsInline
                             loop
                             muted
