e4f70790d4b02204484392cd2b011faba944dbfd
s.feli <s.feli@DESKTOP-8HQAAOS>
2023-11-06 16:09:19 +0330

    fix mobile bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { Fragment, useState } from "react";
 import Header from "./Components/Header";
-import Home from "./Components/Home";
-import Home2 from './pages/Home';
-import Features from "./Components/Features";
-import Login from "./Components/Login";
+import Home from './pages/Home';
 import Footer from "./Components/Footer";
-import TypeIn from "./Components/TypeIn";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import ContactPage from "./Components/ContactPage";
 
@@ -16,24 +12,11 @@ function App() {
     <Router>
       <Routes>
         <Route
-          path="/old"
-          element={
-            <Fragment>
-              <Header visible={!isHeaderHidden} hideFirst={true} />
-              <Home onToggleVisibility={setIsHeaderHidden} />
-              <TypeIn />
-              <Features />
-              <Login />
-              <Footer />
-            </Fragment>
-          }
-        />
-        <Route
           path="/"
           element={
             <Fragment>
               <Header visible={!isHeaderHidden} hideFirst={true} />
-              <Home2 />
+              <Home />
             </Fragment>
           }
         />
